05d7603038ae70d651aab86c5118588521e71488
pchr-srf <philipp.christen@srf.ch>
2023-06-16 17:25:52 +0100

    update reference in code as well ;_;

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import A11yDialogLib from 'a11y-dialog'
+import A11yDialogLib from 'srf-a11y-dialog'
 
 const useIsMounted = () => {
   const [isMounted, setIsMounted] = React.useState(false)
